84c0ee92f07f284d9bf09527c26516678fcece8e
devray2077 <devray2077@gmail.com>
2024-01-14 20:38:20 +0300

    added ui effect library

---
+++ Assets/3rdParty/UIEffect-upm/Scripts/UIGraphicsOffset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace UnityEngine.UI {
+
+    [AddComponentMenu("UI/Effects/GraphicsOffset", 15)]
+    public class UIGraphicsOffset : BaseMeshEffect {
+        public Vector2 offset;
+
+        private static readonly List<UIVertex> verts = new List<UIVertex>(128);
+
+        public override void ModifyMesh(VertexHelper vh) {
+            if(!this.IsActive())
+                return;
+
+            List<UIVertex> list = new List<UIVertex>();
+            vh.GetUIVertexStream(list);
+
+            for(int i = 0; i < list.Count; i++) {
+                UIVertex vert = list[i];
+                vert.position += (Vector3) offset;
+                list[i] = vert;
+            }
+
+            vh.Clear();
+            vh.AddUIVertexTriangleStream(list);
+        }
+    }
+}
